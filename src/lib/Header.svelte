9458c8e281d7c4c43a219e336adc819add183512
ZandarB <BEKEZA1@student.op.ac.nz>
2024-10-15 11:11:39 +1300

    Added Title

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,19 +4,21 @@
 </script>
 
 <header>
-    <h1>My Class WOrk Website</h1>
+    <h1>My Class Work Website</h1>
     <nav>
         <ul>
             <li><a href="/">Home</a></li> 
             <li><a href="/">About</a></li>
             <li><a href="/">Contact</a></li>
         </ul>
-
-
     </nav>
 </header>
 
 <style>
 
+h1 + nav {
 
+    font-weight: bolder;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
+}
 </style>
