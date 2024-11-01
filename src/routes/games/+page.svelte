85c9065c2391bf9c840aa58ae5397e76714a56cd
ZandarB <BEKEZA1@student.op.ac.nz>
2024-11-01 13:07:57 +1300

    added game and begun polish

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,115 +1,171 @@
 <script>
     import Footer from '$lib/Footer.svelte';
     import Header from '$lib/Header.svelte';
-    
-    let operators = []; // To store all operators
+
+    let operators = []; 
     let selectedOperator = null;
     let operatorE0Art = '';
-    let operatorE2Art = '';
     let feedback = '';
+    let isLoading = true;
+    let correctAnswers = 0;
+    let selectedClass = '';
     let classes = [
-        "Class A",
-        "Class B",
-        "Class C",
-        "Class D",
-        "Class E",
-        "Class F",
-        "Class G",
-        "Class H"
-    ]; // Placeholder classes
-    
-    // Function to fetch all operators
+        "Vanguard",
+        "Guard",
+        "Defender",
+        "Sniper",
+        "Caster",
+        "Medic",
+        "Specialist",
+        "Supporter"
+    ]; 
+
     async function fetchOperators() {
-        try {
-            const response = await fetch(`https://api.rhodesapi.com/api/operator/`);
-            if (response.ok) {
-                operators = await response.json();
-                console.log("Successfully fetched all operators");
-            } else {
-                console.log("Error fetching operators");
-                feedback = 'There was an error fetching operators.';
-            }
-        } catch (err) {
-            console.error(err);
+        isLoading = true;
+        const response = await fetch(`https://api.rhodesapi.com/api/operator/`);
+        if (response.ok) {
+            operators = await response.json();
+            selectRandomOperator();
+        } else {
             feedback = 'There was an error fetching operators.';
         }
+        isLoading = false;
     }
     
-    // Function to select a random operator
     function selectRandomOperator() {
         if (operators.length > 0) {
             const randomIndex = Math.floor(Math.random() * operators.length);
             selectedOperator = operators[randomIndex];
             operatorE0Art = selectedOperator.art[0].link; 
-            operatorE2Art = selectedOperator.art[2]?.link || ''; // Optional chaining for E2 art
-            feedback = ''; // Clear any previous feedback
+            feedback = ''; 
+            selectedClass = '';
         } else {
-            feedback = 'No operators available to select.';
+            feedback = 'No operators available.';
         }
     }
     
-    // Function to check the answer
-    function checkAnswer(selectedClass) {
-        const correctClass = selectedOperator.class.join(', '); // Assuming class is an array
-        if (selectedClass === correctClass) {
-            feedback = 'Correct!';
+    function checkAnswer(className) {
+        const correctClass = selectedOperator.class[0];
+        if (className === correctClass) {
+            correctAnswers++;
+            feedback = 'Correct! Total Correct Answers: ' + correctAnswers;
+            setTimeout(selectRandomOperator, 2000);
         } else {
-            feedback = 'Incorrect. The correct class is: ' + correctClass;
+            feedback = 'Incorrect. The correct class is: ' + correctClass + '. The game has been reset.';
+            correctAnswers = 0;
+            selectedOperator = null;
+            operatorE0Art = '';
+            selectedClass = '';
+            setTimeout(selectRandomOperator, 2000);
         }
     }
-    
-    // Call fetchOperators when the component mounts
+
     fetchOperators();
-    </script>
-    
-    <div class="content">
-        <div class="searchbar">
-            <h2>Operator Quiz</h2>
-            <br>
-            <button class="searchbutton" on:click={selectRandomOperator}>Get Random Operator</button>
-            
-            {#if selectedOperator}
-                <div class="operatorSpecifics">
+</script>
+
+<div class="content">
+    <div class="searchbar">
+        <h2>Operator Quiz</h2>
+        <br>
+        
+        {#if isLoading}
+            <p>Loading...</p>
+        {:else}
+            <div class="operatorSpecifics">
+                {#if selectedOperator}
                     <p>
                         Name: {selectedOperator.name}<br>
                         Rarity: {selectedOperator.rarity}* <br>
-                        
-                        {#if operatorE0Art} 
-                            E0 Art: 
-                            <br>
-                            <img src={operatorE0Art} alt={selectedOperator.name + " E0 IMG"} width="500" height="500"/> 
-                        {/if} 
-    
+                        E0 Art: 
                         <br>
-    
-                        {#if operatorE2Art} 
-                            E2 Art:                    
-                            <br>
-                            <img src={operatorE2Art} alt={selectedOperator.name + " E2 IMG"} width="500" height="500"/>                     
-                        {/if}
+                        <img src={operatorE0Art} alt={selectedOperator.name + " E0 IMG"} width="500" height="500"/> 
                     </p>
-                </div>
-    
+                {/if}
+            </div>
+
+            {#if selectedOperator}
                 <h3>What is the class of this operator?</h3>
-                {#each classes as className}
+                <div class ="radioButtonContainer">
+                    {#each classes as className}
                     <label>
                         <input type="radio"
                             name="operatorClass"
                             value={className}
+                            bind:group={selectedClass}
                             on:change={() => checkAnswer(className)} />
                         {className}
                     </label>
-                {/each}
-                <p>{feedback}</p>
-            {:else if feedback}
-                <div class="operatorSpecifics">
-                    <p class="error">{feedback}</p>
-                </div>
+            {/each}
+            </div>
+             {#if feedback.startsWith('Correct')}
+                    <p class="success">{feedback}</p>
+                {:else}
+                <p class="error">{feedback}</p>
+                {/if}            
             {/if}
-        </div>
+        {/if}
+        <h3>Welcome to my game of knowledge and memory!. </h3>
+        <p>This quiz will test you on your knowledge of AK operators and their class, if you get the answer correct, Great! your score will add up infinitely. Get one wrong and you will be reset back to 0! Good luck!</p>
+        <p>Please be aware that a select few operators do not have a full HD transparent art, and as such may look a bit skewed. Also when a new question is loading please allow a few seconds to answer, as the image may be loading.</p>
     </div>
-    
-    <style>
-    /* Your existing CSS styles remain unchanged */
-    </style>
-    
+</div>
+
+<style>
+div > p, h3 {
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: bold;
+    color: rgb(75, 75, 75);
+}
+
+.searchbar {
+    margin: 10px;
+    text-align: center;
+    padding: 10px;
+}
+
+.content {
+    position: relative;
+    z-index: 1;
+    background-color: lightgray; 
+    width: 750px;
+    padding: 20px; 
+    margin: 20px auto 0;    
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    border: black solid 2px;
+    border-radius: 7px;
+}
+
+h2 {
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: bold;
+    color: rgb(75, 75, 75);
+}
+
+.error {
+    color: red;
+}
+
+.success {
+    color: green;
+}
+
+label {
+    display: flex; 
+    align-items: center; 
+    flex-wrap: wrap; 
+    margin: 5px 0; 
+    cursor: pointer; 
+    font-family: 'Noto Sans', sans-serif; 
+    font-size: 16px; 
+    color: rgb(75, 75, 75); 
+    max-width: 200px; /* Set a maximum width for the label */
+}
+
+input[type="radio"] {
+    margin-right: 10px; 
+    accent-color: #007bff; 
+}
+
+</style>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,65 +1,48 @@
 <script>
-import Footer from '$lib/Footer.svelte';
-import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+    import Header from '$lib/Header.svelte';
 
+    let userInput = '';
+    let operatorData = '';
+    let operatorClass = [];
+    let operatorE0Art = '';
+    let operatorE2Art = '';
+    let error = '';
+    let isLoading = false;
 
-let userInput = '';
-let operatorData = '';
-let operatorClass = [,,,];
-let operatorE0Art = '';
-let operatorE2Art = '';
-let error = '';
+    async function getOperators(name) {
+        isLoading = true;
+        const response = await fetch(`https://api.rhodesapi.com/api/operator/${name}/`);
 
-async function getOperators(name)
-{
-    const allOps = await fetch(`https://api.rhodesapi.com/api/operator/`);
-    const allOpsData = await allOps.json();
-    console.log(allOpsData)
-
-    const response = await fetch(`https://api.rhodesapi.com/api/operator/${name}/`);
-
-   
-    if (response.ok)
-    {
-        const data = await response.json();
-        operatorClass = data.class;
-        operatorData = data;
-        operatorE0Art = data.art[0].link; 
-        if (data.art[2] && data.art[2].link) {
-            operatorE2Art = data.art[2].link; 
+        if (response.ok) {
+            const data = await response.json();
+            operatorClass = data.class;
+            operatorData = data;
+            operatorE0Art = data.art[0].link; 
+            operatorE2Art = data.art[2] && data.art[2].link ? data.art[2].link : '';
+            error = '';
+            console.log("Successful fetch");
         } else {
-            operatorE2Art = ''; // Clear E2 art if it doesn't exist
+            console.log("Error fetching operators. Invalid name entered.");
+            error = 'There was an Error, Please enter a valid Name.';
         }
-        error = '';
-        console.log("Successful fetch")
+        isLoading = false;
     }
-    else
-    {
-        console.log("Error fetching operators. Invalid name entered.")
-        error = 'There was an Error, Please enter a valid Name.';
-    }
-}
 
-function search()
-{
-    operatorE0Art = '';
-    operatorE2Art = '';
-    operatorData = '';
-    operatorClass = '';
+    function search() {
+        operatorE0Art = '';
+        operatorE2Art = '';
+        operatorData = '';
+        operatorClass = '';
 
-    if (userInput){
-        getOperators(userInput)
-        
+        if (userInput) {
+            getOperators(userInput);
+        }
     }
-    else
-    {
-    }
-}
 </script>
 
-
 <div class="content">
-    <div class = "searchbar">
+    <div class="searchbar">
         <h2 for="textbox">Search for Operators</h2>
         <br>
         <input
@@ -70,25 +53,28 @@ function search()
         />
         <br>
         <button class="searchbutton" on:click={search}>Click to fetch data (If available)</button>
-        {#if operatorData}
+        
+        {#if isLoading}
+            <p>Loading...</p>
+        {:else if operatorData}
             <div class="operatorSpecifics">
                 <p>
                     Name: {operatorData.name}<br>
                     Rarity: {operatorData.rarity}* <br>
                     Class: {operatorClass.join(', ')} <br>
                     
-                    {#if operatorE0Art[0]} 
+                    {#if operatorE0Art} 
                         E0 Art: 
                         <br>
-                        <img src={operatorE0Art} alt={operatorData.name + " E0 IMG"} width="500" height="500px"/> 
+                        <img src={operatorE0Art} alt={operatorData.name + " E0 IMG"} width="500" height="500"/> 
                     {/if} 
 
                     <br>
 
-                    {#if operatorE2Art[2]} 
+                    {#if operatorE2Art} 
                         E2 Art:                    
                         <br>
-                        <img src={operatorE2Art} alt={operatorData.name + " E2 IMG"} width="500px" height="500px"/>                     
+                        <img src={operatorE2Art} alt={operatorData.name + " E2 IMG"} width="500" height="500"/>                     
                     {/if}
                 </p>            
             </div>
@@ -97,42 +83,35 @@ function search()
                 <p class="error">{error}</p>
             </div>
         {/if}
+        <p>Here on this page you can search for any operator (Up to a certain point, currently the latest 6* is Ulpianus). And the website will display data related to the operator, assuming they exist.</p>
     </div>
 </div>
 
-
 <style>
-
-div > p{
-
+div > p {
     font-family: 'Noto Sans', sans-serif;
     font-weight: bold;
-    src: url(https://fonts.gstatic.com/s/notosans/v36/o-0ZIpQlx3QUlC5A4PNr4C5OaxRsfNNlKbCePevttHOmDyw.woff2) format('woff2');
-    unicode-range: U+0460-052F, U+1C80-1C8A, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;    
-    color: rgb(75,75,75);
-
+    color: rgb(75, 75, 75);
 }
 
-.searchbar{
+.searchbar {
     margin: 10px;
     text-align: center;
     padding: 10px;
 }
-    
-.searchbutton{
+
+.searchbutton {
     width: 250px;
     height: 40px;
-    margin:10px;
+    margin: 10px;
     outline: black solid 1px;
 }
 
-
-input{
-    width:500px;
+input {
+    width: 500px;
     height: 40px; 
     outline: black solid 1px;
 }
-
 
 .content {
     position: relative;
@@ -146,19 +125,15 @@ input{
     align-items: center; 
     border: black solid 2px;
     border-radius: 7px;
-
 }
 
-h2{
+h2 {
     font-family: 'Noto Sans', sans-serif;
     font-weight: bold;
-    src: url(https://fonts.gstatic.com/s/notosans/v36/o-0ZIpQlx3QUlC5A4PNr4C5OaxRsfNNlKbCePevttHOmDyw.woff2) format('woff2');
-    unicode-range: U+0460-052F, U+1C80-1C8A, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;    
-    color: rgb(75,75,75);
-    ;
+    color: rgb(75, 75, 75);
 }
 
-.error{
+.error {
     color: red;
 }
 </style>
